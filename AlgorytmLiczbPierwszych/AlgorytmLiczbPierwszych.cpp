7482d159b5301f0835555fdedbd685ecd8d32766
levandovsky11 <mod2004bl@gmail.com>
2022-03-04 14:08:11 +0100

    04.03.2022

---
+++ AlgorytmLiczbPierwszych/AlgorytmLiczbPierwszych.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int liczba;
+
+    cout << "podaj liczbe" <<endl;
+    cin >> liczba;
+
+    cout << "Jest liczba pierwsza  "<< endl;
+}
