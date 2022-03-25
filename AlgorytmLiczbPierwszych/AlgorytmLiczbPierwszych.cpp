127deac2e9cb204f41d4bd9b5a3ad34ca4ddce0e
levandovsky11 <mod2004bl@gmail.com>
2022-03-25 14:09:43 +0100

    Updated

--- AlgorytmLiczbPierwszych/AlgorytmLiczbPierwszych.cpp
+++ AlgorytmLiczbPierwszych/AlgorytmLiczbPierwszych.cpp
@@ -1,12 +1,34 @@
-﻿#include <iostream>
+﻿#include<iostream>
 using namespace std;
 
 int main()
 {
-    int liczba;
+	int i;
+	int	j;
+	int	liczenie = 1;
+	int	b = 0;
+	int ilosc = 0;
 
-    cout << "podaj liczbe" <<endl;
-    cin >> liczba;
+	cout << "podaj ile chcesz liczb pierwszych\n" <<endl;
+	cin >> ilosc;
+	for (i = 3; i > 0; ++i)
+	{
+		for (j = 2; j <= i / 2; ++j)
+		{
+			if (i % j == 0) {
+				b = 1;
+				break;
+			}
+		}
+		if (b == 0)
+		{
+			cout << "\n" << i;
+			liczenie++;
+		}
+		b = 0;
+		if (liczenie == ilosc+1)
+			break;
+	}
 
-    cout << "Jest liczba pierwsza  "<< endl;
+	return 0;
 }
