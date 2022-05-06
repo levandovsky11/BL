e9bb70145a7f04a9836e964d8410f125ee33b7c8
levandovsky11 <mod2004bl@gmail.com>
2022-05-06 13:45:58 +0200

    updateznowu

---
+++ CiagFibonacciego/CiagFibonacciego.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ilosc;
+    int f = 0;
+    int f1 = 1;
+    int f2;
+
+    cout << "podaj ilosc liczb do wypisania: " <<endl;
+    cin >> ilosc;
+
+    for (int i = 0; i <= ilosc; i++) {
+        if (i > 1) {
+            f2 = f + f1;
+            f = f1;
+            f1 = f2;
+        }
+        else {
+            f2 = i;
+        }
+        cout << f2 << endl;
+    }
+    return 0;
+}
---
+++ niedziałaSzyfrCezara/SzyfrCezara.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void szyfrowanie(char wyraz[], int klucz);
+
+int main()
+{
+    //to działa
+    //char tar[200];
+   // cin >> tar;
+   // tar[0] += 2;
+    //cout << tar << endl;
+
+    int klucz;
+    char wyraz[200];
+
+    cout << "podaj klucz" << endl;
+    cin >> klucz;
+    cout << "podaj wyraz" << endl;
+    cin >> wyraz;
+    szyfrowanie(wyraz, klucz);
+    //wyraz[0] += 2;
+    //cout << wyraz << endl;
+
+}
+
+void szyfrowanie(char wyraz[], int klucz) {
+    int dlugosc;
+    dlugosc = strlen(wyraz);
+    if (!(klucz >= -26 && klucz <= 26)) {
+        return;
+    }
+    
+    if (klucz >= 0) {
+
+    }
+
+    
+}
