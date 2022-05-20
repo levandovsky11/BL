6e500892b9a5d635c6802662f5c34776180f9003
levandovsky11 <mod2004bl@gmail.com>
2022-05-20 13:42:41 +0200

    update kolejny

---
+++ Wyszukiwanie binarne/Wyszukiwanie binarne.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+int szukanie(int liczba, int t[]) {
+
+    int a = 0;
+    int b = 15;
+    int sr = (a + b) / 2;
+
+    while (a <= b)
+    {
+        if (t[sr] == liczba)
+        {
+            return sr;
+        }
+        if (t[sr] > liczba)
+        {
+            b = sr - 1;
+        }
+        else
+        {
+            a = sr + 1;
+        }
+        sr = (a + b) / 2;
+    }
+    return -1;
+}
+int main()
+{
+    int liczba;
+    int a = 0;
+    int b = 7;
+    int sr = (a + b) / 2;
+    int t[15] = { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47 };
+
+    cout << "------------------------------------------------------------------------" << endl;
+    cout << "Podaj liczbe ktora chcesz znalezc: ";
+    cin >> liczba;
+    cout << "------------------------------------------------------------------------" << endl;
+    cout << "Liczba " << liczba << " wystepuje w zbiorze w komorce o indeksie " << szukanie(liczba, t) << endl;
+    cout << "------------------------------------------------------------------------" << endl;
+}
